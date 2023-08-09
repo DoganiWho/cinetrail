ba21876bb30b1065d24a493ede890da33ed4478c
Leslie Skog <96683644+DoganiWho@users.noreply.github.com>
2023-08-09 05:34:13 +0000

    created movieTypes and updated Slider component

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,21 +1,50 @@
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
+import { Movie } from "../movieTypes";
 
 export default function Slider() {
+  const [upcoming, setUpcoming] = useState<Movie[]>([]);
 
-    const [upcoming, setUpcoming] = useState<string[]>([]);
+  useEffect(() => {
+    axios
+      .get(
+        `${import.meta.env.VITE_API_BASE_URL}upcoming?api_keys${
+          import.meta.env.VITE_APP_API_KEY
+        }`
+      )
+      .then((res) => {
+        const movies: Movie[] = res.data.results;
+        setUpcoming(movies);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-    useEffect(() => {
-        axios.get(`${import.meta.env.VITE_API_BASE_URL}upcoming?api_keys${import.meta.env.VITE_APP_API_KEY}`)
-        .then(res => console.log(res.data.results))
-        .catch(err => console.error(err))
-    }, [])
+  const sliderStyle = {
+    backgroundImage: `url(${import.meta.env.VITE_API_BASE_IMG_URL}${
+      upcoming[0]?.backdrop_path
+    })`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    height: "60vh",
+    width: "100%",
+  };
 
-    return (
-        <div>
-            {upcoming.map(movie => {
-            <p>{movie.title}</p>
-            })}
+  return (
+    <div style={sliderStyle}>
+      {upcoming.map((movie) => (
+        <div key={movie.id}>
+          <h2>{movie.title}</h2>
+          <p>{movie.overview}</p>
+          <p>Release Date: {movie.release_date}</p>
+          {movie.poster_path && (
+            <img
+              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              alt={`${movie.title} Poster`}
+            />
+          )}
         </div>
-    )
+      ))}
+    </div>
+  );
 }
---
+++ src/components/movieTypes.ts
@@ -0,0 +1,9 @@
+export interface Movie {
+    id: number;
+    title: string;
+    overview: string;
+    release_date: string;
+    poster_path: string | null;
+    backdrop_path: string | null;
+    // Add more fields as needed
+}
